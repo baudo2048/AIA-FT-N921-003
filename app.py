ce8b6fd1705b0449d69b0c6f768ba217a61cfae6
baudo2048 <baudo81@gmail.com>
2021-11-17 22:26:57 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,23 @@
 from flask import Flask
+import json
+
+
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return 'Hello Strat thinking!'
+
+@app.route('/events')
+def events():
+    x = {
+        "name": "John",
+        "age": 30,
+        "city": "New York"
+    }
+    
+    
+    return json.dumps(x)
 
 if __name__ == '__main__':
     app.run()
