2a6e8e99ccabff316da6823d85b48a1411378174
baudo2048 <baudo81@gmail.com>
2021-11-17 23:01:53 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask import render_template
 import json
 
 
@@ -6,10 +7,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    try:
-	    return render_template('index.html')
-	except Exception as e:
-		return str(e)
+	return render_template('index.html')
 
 @app.route('/events')
 def events():
